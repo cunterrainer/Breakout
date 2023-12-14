be14238dd602c4c93e799330f42c5b92a5a744a6
pyvyx <pyvyx@web.de>
2023-12-14 12:12:09 +0100

    Fixed typo in comment

--- Breakout/src/main.c
+++ Breakout/src/main.c
@@ -156,7 +156,7 @@ int ball_move(struct Ball* ball, Rectangle paddle, float dt)
     }
     else if (CheckCollisionCircleRec(ball->center, ball->radius, paddle) && ball->direction.y > 0)
     {
-        // if you hit the ball in the first 35 % the ball goes back the direction reverses
+        // if you hit the ball in the first 25 % the ball goes back the direction reverses
         // if going from left to right                                                    if going from right to left
         if ((ball->direction.x > 0 && ball->center.x < paddle.x + paddle.width * 0.25) || (ball->direction.x < 0 && ball->center.x > paddle.x + paddle.width * 0.75))
         {
